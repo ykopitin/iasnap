91bf1136df4bf45fc86391f994b8eea33795b6d3
prk <prk@prknt>
2014-07-21 00:21:25 +0300

    Подключение базы, включение gii

--- protected/config/main.php
+++ protected/config/main.php
@@ -19,15 +19,12 @@ return array(
 	),
 
 	'modules'=>array(
-		// uncomment the following to enable the Gii tool
-		/*
 		'gii'=>array(
 			'class'=>'system.gii.GiiModule',
-			'password'=>'Enter Your Password Here',
+			'password'=>'iasnap',
 			// If removed, Gii defaults to localhost only. Edit carefully to taste.
 			'ipFilters'=>array('127.0.0.1','::1'),
-		),
-		*/
+		),		
 	),
 
 	// application components
@@ -51,15 +48,15 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=cnap_portal',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'iasnap',
+			'password' => 'iasnap98',
 			'charset' => 'utf8',
 		),
-		*/
+		/*
 		'errorHandler'=>array(
 			// use 'site/error' action to display errors
 			'errorAction'=>'site/error',
