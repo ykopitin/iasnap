910d6877717402c5d15bf511a3ddbdbe36e56016
prk <prk@prknt>
2014-07-22 12:47:49 +0300

    Подключение базы, включение gii

--- protected/config/console.php
+++ protected/config/console.php
@@ -15,15 +15,15 @@ return array(
 			'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
 		),
 		// uncomment the following to use a MySQL database
-		/*
+		
 		'db'=>array(
-			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
+			'connectionString' => 'mysql:host=localhost;dbname=cnap_portal',
 			'emulatePrepare' => true,
-			'username' => 'root',
-			'password' => '',
+			'username' => 'iasnap',
+			'password' => 'iasnap98',
 			'charset' => 'utf8',
 		),
-		*/
+		/**/
 		'log'=>array(
 			'class'=>'CLogRouter',
 			'routes'=>array(
