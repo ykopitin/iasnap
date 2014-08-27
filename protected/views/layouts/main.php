84b90e46384ce1e6bc19dc65231e2b164879de34
prk <prk@prknt>
2014-08-27 14:08:13 +0300

    Добавлено меню администратора для авторизированных пользователей (для удобства)

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -32,6 +32,7 @@
 				array('label'=>'Home', 'url'=>array('/site/index')),
 				array('label'=>'About', 'url'=>array('/site/page', 'view'=>'about')),
 				array('label'=>'Contact', 'url'=>array('/site/contact')),
+				array('label'=>'Админка', 'url'=>array('/admin'), 'visible'=>!Yii::app()->user->isGuest),
 				array('label'=>'Login', 'url'=>array('/site/login'), 'visible'=>Yii::app()->user->isGuest),
 				array('label'=>'Logout ('.Yii::app()->user->name.')', 'url'=>array('/site/logout'), 'visible'=>!Yii::app()->user->isGuest)
 			),
