1a8e9aa130a2b86be0107e8a8803620b1167d9ad
prk <prk@prknt>
2014-10-22 02:45:15 +0300

    Исправлены режимы просмотра у файла и картинки

--- protected/modules/mff/views/formview/view/_image.php
+++ protected/modules/mff/views/formview/view/_image.php
@@ -2,7 +2,7 @@
 if ($scenario=="update" || $scenario=="insert") {
     echo $form->fileField($modelff,strtolower($data->name));
 }
-if (($scenario=="update" || $scenario=="view") &&  ($modelff->hasAttribute(strtolower($data->name)) && ($modelff->getAttribute(strtolower($data->name))!=null))) {
+if (($scenario=="update" || $scenario=="view") && ($modelff->getAttribute(strtolower($data->name))!=null)) {
     echo CHtml::image($this->createUrl("getimage",array("id"=>$modelff->id,"name"=>$data->name)),'',array("style"=>"width:70%"));
 }
 ?>
